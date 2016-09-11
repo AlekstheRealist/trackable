0ea9c8117bdae8c57ff8c27dd0616234c4108cc5
Aleks Real <aleksanderreal@gmail.com>
2016-09-11 01:06:42 +0000

    Added home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Trackable::Application.routes.draw do
   devise_for :users
-  root "weeks#index"
+  root "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
